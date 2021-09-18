215ea722b36276461b576ef875325f0f712cef07
dmanojlovic <manojlovic.david@gmail.com>
2021-09-18 18:19:28 -0400

    Co-authored-by: Zogbi, Daniel <dzogbi@purdue.edu>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1/>Hi there!
+      <h2/>Hello there yep there
     </div>
-  );
+  )
 }
+//function dropdown()){
+ // return(
+   // <nav className="dropdown">
+   //   <ul className="dropdown-nav"></ul>
+   // </nav>
+ // );
+//}
 
 export default App;
