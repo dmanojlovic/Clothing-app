dfe055334d44c4f45b619159835a52be66a511fd
izatwin <88566094+izatwin@users.noreply.github.com>
2021-09-19 06:07:14 -0400

    brandsizes

---
+++ public/data/brandSizes.js
@@ -0,0 +1,18 @@
+export const Uniqlo = {
+    XXS: {start:31, end:33 },
+    XS: {start:32, end:35 },
+    S: {start:35, end:38 },
+    M: {start:38, end:41 },
+    L: {start:41, end:44 },
+    XL: {start:44, end:47 },
+    XXL: {start:47, end:50 }
+}
+
+export const Levis = {
+    XS: {start:32, end: 34},
+    S: {start:35, end: 37},
+    M: {start:38, end: 40},
+    L: {start:41, end: 43},
+    XL: {start:44, end: 46},
+    XXL: {start:47, end: 49}
+}
